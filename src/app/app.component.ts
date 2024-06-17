df8a474eaad337183d633fea17c7773f00e3c3d9
Avi <avicreationstudio@gmail.com>
2024-06-17 22:20:16 +0530

    updated route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GlobalHeaderComponent } from './shared/components/global-header/global-header.component';
+import { GlobalFooterComponent } from './shared/components/global-footer/global-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    GlobalHeaderComponent,
+    GlobalFooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { GameDetailsComponent } from './pages/game-details/game-details.component';
+import { PlayGameComponent } from './pages/play-game/play-game.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'landing', component: LandingPageComponent },
+    { path: 'home', component: HomePageComponent },
+    { path: 'game-details', component: GameDetailsComponent, canActivate: [authGuard] },
+    { path: 'play/:game-id', component: PlayGameComponent, canActivate: [authGuard] },
+    { path: 'page-not-found', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+    { path: '', redirectTo: '/landing', pathMatch: 'full' }, // redirect to `first-component`
+    { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
