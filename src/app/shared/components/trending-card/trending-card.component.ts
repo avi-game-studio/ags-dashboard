eb41ce07122002fd40cfc229b7188655d4a2ac7b
Hariram <155644654+Hariram-HR@users.noreply.github.com>
2024-06-18 21:48:03 +0530

    update

--- src/app/shared/components/trending-card/trending-card.component.ts
+++ src/app/shared/components/trending-card/trending-card.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input ,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trending } from './trending-card.model';
 
 @Component({
   selector: 'app-trending-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './trending-card.component.html',
   styleUrl: './trending-card.component.css'
 })
-export class TrendingCardComponent {
-
+export class TrendingCardComponent  implements OnInit  {
+  @Input() trendings: trending[] = [];
+  title:string='Trending:';
+  ngOnInit() {
+    this.trendings = [
+      {
+        id: 1,
+        game_name:"one piece",
+        imgURL:"https://i.pinimg.com/736x/65/92/39/6592392e1bbffb904ac76bd544dae3b8.jpg"
+      },
+      {
+        id: 1,
+        game_name:"one piece",
+        imgURL:"https://i.pinimg.com/736x/65/92/39/6592392e1bbffb904ac76bd544dae3b8.jpg"
+      },
+      {
+        id: 1,
+        game_name:"one piece",
+        imgURL:"https://i.pinimg.com/736x/65/92/39/6592392e1bbffb904ac76bd544dae3b8.jpg"
+      },
+      {
+        id: 1,
+        game_name:"one piece",
+        imgURL:"https://i.pinimg.com/736x/65/92/39/6592392e1bbffb904ac76bd544dae3b8.jpg"
+      }
+    ];
+  }
+  
+  
 }
---
+++ src/app/shared/components/trending-card/trending-card.model.ts
@@ -0,0 +1,5 @@
+export interface trending {
+    id:number;
+    game_name:string;
+    imgURL:any;
+}
