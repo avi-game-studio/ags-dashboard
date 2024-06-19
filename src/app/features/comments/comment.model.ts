5f35b7a931b7a7fa14f27cf1b23381ea1053e467
parunkumar645 <parunkumar645@gmail.com>
2024-06-19 12:24:03 +0530

    add like dislike profileimage.

--- src/app/features/comments/comment.model.ts
+++ src/app/features/comments/comment.model.ts
@@ -2,8 +2,11 @@ export interface Comment {
   id: number;
   text: string;
   author: string;
+  profilePicture: string; // New property for profile picture URL
   likes: number;
   dislikes: number;
+  likedBy: Set<string>;
+  dislikedBy: Set<string>;
   replies: Comment[];
   showReplies: boolean;
   showReplyBox: boolean;
