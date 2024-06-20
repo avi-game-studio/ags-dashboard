0e24f7c56d8a113a59a27e12b8fca5a4f8145cac
saravanabalaa <133802179+PeradheepY@users.noreply.github.com>
2024-06-20 16:18:30 +0530

    update

--- src/app/shared/components/trending-card/trending-card.model.ts
+++ src/app/shared/components/trending-card/trending-card.model.ts
@@ -1,14 +1,6 @@
-export interface products {
-    {
-        id: '1000',
-        code: 'f230fh0g3',
-        name: 'Bamboo Watch',
-        description: 'Product Description',
-        image: 'bamboo-watch.jpg',
-        price: 65,
-        category: 'Accessories',
-        quantity: 24,
-        inventoryStatus: 'INSTOCK',
-        rating: 5
-    },
-}
+export interface product {
+    id: number;
+    GameName: string;
+    image: string;
+  }
+  
