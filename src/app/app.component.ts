ef4388fbff1be15d8b766626d796251b925fdd6e
Eniyaraj  L <159445604+lovelyraj69@users.noreply.github.com>
2024-06-21 06:49:42 +0530

    feat : create custom date pipe..! (#16)
    Co-authored-by: Avi <avicreationstudio@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GlobalHeaderComponent } from './shared/components/global-header/global-header.component';
 import { GlobalFooterComponent } from './shared/components/global-footer/global-footer.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
     GlobalHeaderComponent,
-    GlobalFooterComponent,
+    GlobalFooterComponent
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ags-dashboard';
