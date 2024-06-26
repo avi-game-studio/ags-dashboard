5abc667ec1a2eca0c771b6c4c265a46e78263cc3
Avi <avicreationstudio@gmail.com>
2024-06-26 21:33:09 +0530

    sample test file

--- src/app/shared/pipes/custom-currency.pipe.spec.ts
+++ src/app/shared/pipes/custom-currency.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { CustomCurrencyPipe } from './custom-currency.pipe';
+import { CurrencyPipe } from '@angular/common';
 
 describe('CustomCurrencyPipe', () => {
   it('create an instance', () => {
-    const pipe = new CustomCurrencyPipe();
+    const currencyPipe = new CurrencyPipe("en-US");
+    const pipe = new CustomCurrencyPipe(currencyPipe);
     expect(pipe).toBeTruthy();
+  });
+  it('testing transform method', () => {
+    const pipe = new CustomCurrencyPipe(new CurrencyPipe("en-US"));
+    let checklist = [
+      [100, "₹100.00"],
+      [null, null],
+      [99, "₹99.00"],
+      [899, "₹899.00"],
+      [2000, "₹2,000.00"],
+      [1999, "₹1,999.00"],
+    ];
+    checklist.forEach(list => {
+      let input: any;
+      let output: any;
+      input = list[0];
+      output = list[1];
+      const result = pipe.transform(input);
+      expect(result).toEqual(output);
+    });
   });
 });
