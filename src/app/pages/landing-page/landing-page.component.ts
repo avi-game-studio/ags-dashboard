763a312a84fb970cf47f990fbd05286a93c2b2cd
Avi <avicreationstudio@gmail.com>
2024-06-27 15:46:13 +0530

    my updated code

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { FeaturedArticlesComponent } from './featured-articles/featured-articles.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [FeaturedArticlesComponent, ProductDetailsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
-
+  companyName: string;
+  slogan: string;
+  description: string;
+  buttons: string[];
+  constructor() {
+    this.companyName = "Avi 👻 Game Studios";
+    this.slogan = "Fun starts here!";
+    this.description = `We provide IT solutions in a game-like manner, 
+                        making every task fun and engaging. Dive into a
+                        world where solving tech problems feels like play, 
+                        and say goodbye to the usual frustrations.`;
+    this.buttons = ['Product details', 'play our game 🎮'];
+  }
 }
